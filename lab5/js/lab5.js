0c665ffca012025cadee5ff5ecf5b33f2b88b80f
cjudy8443 <cjudy@csumb.edu>
2022-09-21 14:53:23 -0700

    changed them

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -21,31 +21,51 @@ earthMoon.planetRadius = 10;
 earthMoon.orbitRadius = 25;
 
 //ADD ADDITIONAL CODE HERE!
-solarSystem.newPlanet(1);
+earthMoon.newPlanet();
+let earthMoon2 = earthMoon.planets[0];
+earthMoon2.color = "pink";
+earthMoon2.planetRadius = 8;
+earthMoon2.orbitRadius = 10;
+
+
+solarSystem.newPlanet();
 let mars = solarSystem.planets[1];
 mars.color = "red";
 mars.planetRadius = 30;
 mars.orbitRadius = 10;
 mars.direction = "counterclockwise"
 
-mars.newPlanet(1);
+mars.newPlanet();
 let marsMoon = mars.planets[0];
 marsMoon.color = "green";
 marsMoon.planetRadius = 7;
 marsMoon.orbitRadius = 57;
 
-solarSystem.newPlanet(1);
+solarSystem.newPlanet();
 let pluto = solarSystem.planets[2];
 pluto.color = "purple"
-pluto.planetRadius = 90
-pluto.orbitRadius = 0
+pluto.planetRadius = 90;
+pluto.orbitRadius = 0;
 
-pluto.newPlanet(1);
+pluto.newPlanet();
 let plutoMoon = pluto.planets[0];
 plutoMoon.color = "yellow";
 plutoMoon.planetRadius = 7;
 plutoMoon.orbitRadius = 100;
 plutoMoon.direction = "counterclockwise"
+
+solarSystem.newPlanet();
+let saturn = solarSystem.planets[3];
+saturn.color = "brown"
+saturn.planetRadius = 10;
+saturn.orbitRadius = 30;
+
+saturn.newPlanet();
+let saturnMoon = saturn.planets[0];
+saturnMoon.color = "white";
+saturnMoon.planetRadius = 3;
+saturnMoon.orbitRadius = 50;
+
 
 //Leave this line here, don't change it! setInterval might be a fun thing to Google!
 setInterval(function () { orbitPlanets(solarSystem);}, 20);
