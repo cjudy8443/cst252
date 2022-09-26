6dfc4075d734e9af67cfc31fb21eff9eada62f6e
cjudy8443 <cjudy@csumb.edu>
2022-09-26 11:01:29 -0700

    chnaged

--- lab6/js/lab6.js
+++ lab6/js/lab6.js
@@ -27,25 +27,39 @@ function formatAndSortNames() {
 
 
 	newArray = [];
+//type here//
+//"arrayOfNames"is being used as a parameter. Changes the functions values.//
+function fixTheNames(arrayOfNames){
+splitNames = arrayOfNames.split(' ');
+firstName = splitNames[0];
+lastName = splitNames[1];
+splitName = lastName +', '+firstName;
+newArray.push(fixedName);
+}
+//They print as function for the parameter//
+fixTheNames(arrayOfNames[0]);
+fixTheNames(arrayOfNames[1]);
+fixTheNames(finalNames);
+
 
 	//Remember to Google what "split" does
-	splitNames = arrayOfNames[0].split(' ');
-	firstName = splitNames[0];
-	lastName = splitNames[1];
-	fixedName = lastName + ', ' + firstName;
-	newArray.push(fixedName);
+	//splitNames = arrayOfNames[0].split(' ');
+	//firstName = splitNames[0];
+	//lastName = splitNames[1];
+	//fixedName = lastName + ', ' + firstName;
+	//newArray.push(fixedName);
 
-	splitNames = arrayOfNames[1].split(' ');
-	firstName = splitNames[0];
-	lastName = splitNames[1];
-	fixedName = lastName + ', ' + firstName;
-	newArray.push(fixedName);
+	//splitNames = arrayOfNames[1].split(' ');
+	//firstName = splitNames[0];
+	//lastName = splitNames[1];
+	//fixedName = lastName + ', ' + firstName;
+	//newArray.push(fixedName);
 
-	splitNames = finalName.split(' ');
-	firstName = splitNames[0];
-	lastName = splitNames[1];
-	fixedName = lastName + ', ' + firstName;
-	newArray.push(fixedName);
+	//splitNames = finalName.split(' ');
+	//firstName = splitNames[0];
+	//lastName = splitNames[1];
+	//fixedName = lastName + ', ' + firstName;
+	//newArray.push(fixedName);
 
 	newArray.sort();
 	finalFixedName = newArray.pop();
